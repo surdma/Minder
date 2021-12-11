b85d639bda58480c6440ed19fb3b59b31d04e836
Oladejo Opeyemi <oladejosodiq@yahoo.com>
2021-12-11 22:23:45 +0100

    built a generic custom ordering filed in this commit

--- courses/models.py
+++ courses/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 
 class Subject(models.Model):
@@ -38,4 +40,41 @@ class Module(models.Model):
 
 
 class Content(models.Model):
-    pass
+    module = models.ForeignKey(Module, related_name="contents", on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, limit_choices_to={'model__': (
+        'text',
+        'video',
+        'image',
+        'file'
+    )})
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')
+
+
+class ItemBase(models.Model):
+    owner = models.ForeignKey(User, related_name='%(class)s_related', on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+
+class Text(ItemBase):
+    content = models.TextField()
+
+
+class File(ItemBase):
+    file = models.FileField(upload_to='files')
+
+
+class Image(ItemBase):
+    image = models.ImageField(upload_to='images')
+
+
+class Video(ItemBase):
+    video = models.URLField()
